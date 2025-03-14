9d17d896a615990e0cf3eb6923177cffa3d17149
limjiheon <lim794@naver.com>
2025-03-15 00:59:20 +0900

    기록 검증기 화면 추가, 검증기 사이즈 수정

--- Assets/Scripts/View/HistoryView.cs
+++ Assets/Scripts/View/HistoryView.cs
@@ -7,18 +7,30 @@ public class HistoryView : View
     [SerializeField] private HistoryCell _historyCellPrefab;
     [SerializeField] private RectTransform _parentTransform;
     [SerializeField] private ScrollRect _scrollRect;
+    [SerializeField] private GameObject _desc;
+    [SerializeField] private VerifierCell _verifierCell;
     
     private List<HistoryCell> _historyCells = new();
-
+    private List<Verifier> _verifiers;
+    
     public override void Clear()
     {
         _scrollRect.verticalNormalizedPosition = 1;
+        _desc.SetActive(true);
+        _verifierCell.gameObject.SetActive(false);
     }
 
+    public override void Init(List<Verifier> verifiers)
+    {
+        _verifiers = verifiers;
+    }
+    
     public void OnEnable()
     {
         _scrollRect.verticalNormalizedPosition = 1;
-
+        _desc.SetActive(true);
+        _verifierCell.gameObject.SetActive(false);
+        
         foreach (var historyCell in _historyCells)
         {
             Destroy(historyCell.gameObject);
@@ -31,6 +43,22 @@ public class HistoryView : View
             var cell = Instantiate(_historyCellPrefab, _parentTransform).GetComponent<HistoryCell>();
             cell.Init(history);
             _historyCells.Add(cell);    
+        }
+    }
+
+    public void OnClickAlphabet(string alphabet)
+    {
+        var verifier = _verifiers.Find(p => p.alphabet.ToString() == alphabet);
+        if (verifier == null)
+        {
+            _desc.SetActive(true);
+            _verifierCell.gameObject.SetActive(false);
+        }
+        else
+        {
+            _desc.SetActive(false);
+            _verifierCell.gameObject.SetActive(true);
+            _verifierCell.Init(_mainController, verifier);
         }
     }
 
